f4b32b909ee45f61460f2673e54778a4410d8c57
eldetulado <eldetulado@gmail.com>
2018-04-13 02:11:33 -0400

    Nuevos AlertsDialogs Yeiii

--- app/src/main/java/com/example/oso/timmon/MainActivity.java
+++ app/src/main/java/com/example/oso/timmon/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.oso.timmon;
 
 import android.content.DialogInterface;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.oso.timmon.data.model.LoginR;
@@ -18,68 +21,143 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Boolean sw = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button registro = findViewById(R.id.btnRegistro);
+        Button ingresar = findViewById(R.id.btnIngresar);
 
+        registro.setOnClickListener(this);
+        ingresar.setOnClickListener(this);
     }
 
-    public void signUp(View view) {
-//        Registrarse
+    private void registroDialogo() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.dialog_sign_up, null);
-        builder.setView(dialogView);
+        final View mView = inflater.inflate(R.layout.dialog_sign_up, null);
+        builder.setView(mView);
+        builder.setCancelable(false);
+        final AlertDialog alert = builder.create();
+        alert.show();
 
-        final EditText nombreCompleto = dialogView.findViewById(R.id.names);
-        final EditText nick = dialogView.findViewById(R.id.user_name);
-        final EditText correo = dialogView.findViewById(R.id.email);
-        final EditText password = dialogView.findViewById(R.id.password);
+        final TextInputLayout tInputNombre = mView.findViewById(R.id.tilNombre);
+        final TextInputLayout tInputNick = mView.findViewById(R.id.tilNick);
+        final TextInputLayout tInputCorreo = mView.findViewById(R.id.tilCorreo);
+        final TextInputLayout tInputPassword = mView.findViewById(R.id.tilPassword);
+        final EditText nombreCompleto = mView.findViewById(R.id.names);
+        final EditText nick = mView.findViewById(R.id.user_name);
+        final EditText correo = mView.findViewById(R.id.email);
+        final EditText password = mView.findViewById(R.id.password);
+        ImageButton btnRegistro = mView.findViewById(R.id.btnDialogRegistro);
 
-
-        builder.setPositiveButton("listo", new DialogInterface.OnClickListener() {
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this,
-                        nombreCompleto.getText().toString()+
-                                nick.getText().toString()+
-                                correo.getText().toString()+
-                                password.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                if (validarRegistro(tInputNombre, tInputNick,tInputCorreo,tInputPassword,
+                        nombreCompleto,nick,correo,password)) {
+                    String datos = nombreCompleto.getText().toString() + "-" +
+                            nick.getText().toString() + "-" +
+                            correo.getText().toString() + "-" +
+                            password.getText().toString();
+                    Toast.makeText(MainActivity.this, datos, Toast.LENGTH_SHORT).show();
+                    alert.dismiss();
+                    sw = true;
+                }
             }
         });
-        builder.show();
     }
+
+    private boolean validarRegistro(TextInputLayout tInputNombre, TextInputLayout tInputNick,
+                                    TextInputLayout tInputCorreo, TextInputLayout tInputPassword,
+                                    EditText nombreCompleto, EditText nick, EditText correo,
+                                    EditText password) {
+        if (nombreCompleto.getText().toString().trim().isEmpty()){
+            tInputNombre.setError("Falta rellenar campo");
+            return false;
+        }
+        if (nick.getText().toString().trim().isEmpty()){
+            tInputNick.setError("Falta rellenar campo");
+            return false;
+        }
+        if (correo.getText().toString().trim().isEmpty()){
+            tInputCorreo.setError("Falta rellenar campo");
+            return false;
+        }
+        if (password.getText().toString().trim().isEmpty()){
+            tInputPassword.setError("Falta rellenar campo");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.btnRegistro :
+                if (sw) {
+                    sw = false;
+                    registroDialogo();
+                }
+                break;
+
+            case R.id.btnIngresar:
+                if (sw){
+                    sw = false;
+                    ingresarDialogo();
+                }
+                break;
+        }
+    }
+
+
     private APIService mAPIService;
-
-    public void signIn(View view) {
-//        Ingresar
+    private void ingresarDialogo() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = this.getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.dialog_sign_in, null);
-        builder.setView(dialogView);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_sign_in, null);
+        builder.setView(mView);
+        builder.setCancelable(false);
+        final AlertDialog alert = builder.create();
+        alert.show();
+        final TextInputLayout tNickCorreo = mView.findViewById(R.id.tilNickCorreo);
+        final TextInputLayout tPassword = mView.findViewById(R.id.tilPassword);
+        final EditText nickCorreo = mView.findViewById(R.id.nickCorreo);
+        final EditText password = mView.findViewById(R.id.password);
+        ImageButton btnIngresar = mView.findViewById(R.id.btnDialogIngresar);
 
-        final EditText correo = dialogView.findViewById(R.id.email);
-        final EditText password = dialogView.findViewById(R.id.password);
-
-
-        builder.setPositiveButton("listo", new DialogInterface.OnClickListener() {
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(View v) {
+                if (validarIngreso(tNickCorreo, tPassword, nickCorreo, password)){
+                    mAPIService = ApiUtils.getAPIService();
+                    sendPost(nickCorreo.getText().toString().trim(), password.getText().toString().trim());
 
-                mAPIService = ApiUtils.getAPIService();
-                sendPost(correo.getText().toString().trim(), password.getText().toString().trim());
-                Toast.makeText(MainActivity.this,
-                        correo.getText().toString()+
-                                password.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
+                    String datos = nickCorreo.getText().toString() + "-" +
+                            password.getText().toString();
+                    Toast.makeText(MainActivity.this, datos, Toast.LENGTH_SHORT).show();
+                    alert.dismiss();
+                    sw = true;
+                }
             }
         });
-        builder.show();
+    }
+
+    private boolean validarIngreso(TextInputLayout tNickCorreo, TextInputLayout tPassword,
+                                   EditText nickCorreo, EditText password) {
+        if (nickCorreo.getText().toString().trim().isEmpty()){
+            tNickCorreo.setError("Falta rellenar campo");
+            return false;
+        }
+        if (password.getText().toString().trim().isEmpty()){
+            tPassword.setError("Falta rellenar campo");
+            return false;
+        }
+        return true;
     }
 
 
